cc432086ec551b963b09d765fd19c15ab06eb9db
jason <jobenmoe@gmail.com>
2017-03-19 19:18:33 -0400

    fresh start

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var server = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+server.use(function (req, res, next) {
+  console.log(req.body) // populated!
+  next()
+});
+
+var port = process.env.PORT || 4000;
+
+//Routing
+server.get('/', function(req, res) {
+
+	res.render('home.ejs');
+
+});
+
+
+//More Setup
+server.use(express.static(__dirname + '/static'));
+
+server.listen(port);
+console.log('jasonki.com running on port ' + port);
